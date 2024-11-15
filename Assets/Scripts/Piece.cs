9c261411b73dc80fd851da7222884df71eb95b64
Elliott Hager <ehager2@kent.edu>
2024-11-14 22:15:24 -0500

    Added in a very basic version of rotation checking

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -133,7 +133,11 @@ public class Piece_Backup : MonoBehaviour {
         //Rotation check
         if (pieceRotation.triggered) {
             //Rotate in increments of 90 deg
+            
             currentPiece.rotation += 90;
+
+            //Update after the rotation
+            piecePartRotationLocationUpdater();
         }
 
         //Soft Drop
@@ -241,6 +245,41 @@ public class Piece_Backup : MonoBehaviour {
 
         return true;
     }
+
+    void piecePartRotationLocationUpdater() {
+        //Clear the locations to allow for new
+        PiecePartLocations.Clear();
+        
+
+
+        //Now update the pice part locations
+        foreach (GameObject part in PieceParts) {
+            //print("Horizontal Location Updater: " + movementDirectionModifier);
+
+            //Calculate the new x and y
+            //X = Row
+            //Y = Column
+            int x = part.GetComponent<PiecePart_Location>().getPartLocation().x;
+            int y = part.GetComponent<PiecePart_Location>().getPartLocation().y;
+
+            //Formula for an 90 deg rotation counter-clock wise
+            //(x,y) =(Rotation)=> (y-1, x + 1)
+            //Rotation point is from the bottom of the piece (lowest point) as center!!!
+
+            int newX = x + 1;
+            int newY = y - 1;
+
+            //Update them
+            part.GetComponent<PiecePart_Location>().updatePartLocation(newY, newX);
+
+            //Update the location in the parts location
+            PiecePartLocations.Add(part.GetComponent<PiecePart_Location>().getPartLocation());
+
+            print(part.name + " AFTER: " + part.GetComponent<PiecePart_Location>().getPartLocation().x);
+        }
+
+    }
+
 
     void piecePartHorizontalLocationUpdater(float movementDirectionModifier) {
         //Clear the locations to allow for new
